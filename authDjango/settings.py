c6bf004032985a4228ab78c1c528976ade4ecf49
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2023-02-02 18:26:47 -0500

    listar tareas realizadas

--- authDjango/settings.py
+++ authDjango/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0b4l4$k4&o^z6n!qe2a))_u=7&_#)!ir(e&wo3m6d$-yclo%q8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+# DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
+
 
 ALLOWED_HOSTS = ["*"]
 
