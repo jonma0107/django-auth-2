a7ce5648905bc4b49f24c074c02effd5ba0ffc76
Jonathan Meza <practicantesena3@ticsocial.com.co>
2023-02-06 17:29:08 -0500

    Frontend with Bootstrap!

--- crud/forms.py
+++ crud/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+# from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
   class Meta:
     model = Task
     fields = ['titulo', 'descripcion']
+    widgets = {
+      'titulo': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Escriba un título para la tarea'}),
+      'descripcion': forms.Textarea(attrs = {'class': 'form-control', 'placeholder': 'Escriba una descripción para la tarea'})
+    }
